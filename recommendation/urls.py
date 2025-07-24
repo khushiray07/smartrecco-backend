2d94e5ca724879503357f7f8ea5546e8dfbaf5dc
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2025-07-24 18:03:30 +0530

    Update urls.py

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import RecommendView
+from .auth_views import RegisterView, LoginView
 
 urlpatterns = [
   path('recommend/', RecommendView.as_view()),
+  path('register/', RegisterView.as_view()),
+  path('login/', LoginView.as_view()),
 ]
