6f2a0abdb1760db498ece30902e515dd7f47cbc9
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2025-07-24 18:14:10 +0530

    Update settings.py

--- smartreco_backend/settings.py
+++ smartreco_backend/settings.py
@@ -102,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
